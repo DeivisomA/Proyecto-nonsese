347de5bc68abbf05ebde5a4e765c68bb0a5170ed
Deivisom Andres Salgado Ignacio <dsalgado@cifpfbmoll.eu>
2020-12-21 20:37:51 +0100

    feat:  console input test

--- src/main/java/eu/bmoll/daw/programming/NonsenseApp.java
+++ src/main/java/eu/bmoll/daw/programming/NonsenseApp.java
@@ -5,13 +5,18 @@ import org.apache.commons.lang3.StringUtils;
 import java.util.Scanner;
 
 public class NonsenseApp {
+    private  static  String EXIT_KEY = "9";
     public static void main(String[] args) {
         System.out.println("hola mundo");
         boolean aux = StringUtils.equals("a", "A");
         System.out.println("hola mundo" + aux);
         Scanner in = new Scanner(System.in);
-        String  s = in.nextLine();
-        System.out.println("you entered string" +s);
+        boolean isExit = false;
+        while (!isExit) {
+            String s = in.nextLine();
+            System.out.println("you entered string" + s);
+            isExit = StringUtils.equals(s, EXIT_KEY);
+        }
         System.out.println("-END-");
     }
 }
